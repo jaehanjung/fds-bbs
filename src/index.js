bc76ca15d24cb7d09737fbcc42bce814a8f258b4
jaehanjung <wjdwogks23@gmail.com>
2018-05-24 11:07:18 +0900

    댓글표시기능

--- src/index.js
+++ src/index.js
@@ -1,62 +1,70 @@
-import axios from 'axios';
+import axios from "axios";
 
 const postAPI = axios.create({
   baseURL: process.env.API_URL
 });
-const rootEl = document.querySelector('.root');
+const rootEl = document.querySelector(".root");
 
 function login(token) {
-  localStorage.setItem('token', token);
-  postAPI.defaults.headers['Authorization'] = `Bearer ${token}`;
-  rootEl.classList.add('root--authed');
+  localStorage.setItem("token", token);
+  postAPI.defaults.headers["Authorization"] = `Bearer ${token}`;
+  rootEl.classList.add("root--authed");
 }
 
 function logout() {
-  localStorage.removeItem('token');
-  delete postAPI.defaults.headers['Authorization'];
-  rootEl.classList.remove('root--authed');
+  localStorage.removeItem("token");
+  delete postAPI.defaults.headers["Authorization"];
+  rootEl.classList.remove("root--authed");
 }
 
 const templates = {
-  postList: document.querySelector('#post-list').content,
-  postItem: document.querySelector('#post-item').content,
-  postContent: document.querySelector('#post-content').content,
-  login: document.querySelector('#login').content,
-  postForm: document.querySelector('#post-form').content,
-}
+  postList: document.querySelector("#post-list").content,
+  postItem: document.querySelector("#post-item").content,
+  postContent: document.querySelector("#post-content").content,
+  login: document.querySelector("#login").content,
+  postForm: document.querySelector("#post-form").content,
+  comments: document.querySelector("#comments").content,
+  commentItem: document.querySelector("#comment-item").content
+};
 
 // 중복코드 함수로 작성
 function render(fragment) {
-  rootEl.textContent = '';
+  rootEl.textContent = "";
   rootEl.appendChild(fragment);
 }
 
 async function indexPage() {
-  const res = await postAPI.get('/posts');
+  const res = await postAPI.get("/posts");
   const listFragment = document.importNode(templates.postList, true);
 
-  listFragment.querySelector('.post-list__login-btn').addEventListener('click', e => {
-    loginPage();
-  })
+  listFragment
+    .querySelector(".post-list__login-btn")
+    .addEventListener("click", e => {
+      loginPage();
+    });
 
-  listFragment.querySelector('.post-list__logout-btn').addEventListener('click', e => {
-    logout();
-    indexPage();
-  })
+  listFragment
+    .querySelector(".post-list__logout-btn")
+    .addEventListener("click", e => {
+      logout();
+      indexPage();
+    });
 
-  listFragment.querySelector('.post-list__new-post-btn').addEventListener('click', e => {
-    postFormPage();
-  })
+  listFragment
+    .querySelector(".post-list__new-post-btn")
+    .addEventListener("click", e => {
+      postFormPage();
+    });
 
   res.data.forEach(post => {
     const fragment = document.importNode(templates.postItem, true);
-    const pEl = fragment.querySelector('.post-item__title');
+    const pEl = fragment.querySelector(".post-item__title");
     pEl.textContent = post.title;
-    pEl.addEventListener('click', e => {
+    pEl.addEventListener("click", e => {
       postContentPage(post.id);
     });
-    listFragment.querySelector('.post-list').appendChild(fragment);
-  })
+    listFragment.querySelector(".post-list").appendChild(fragment);
+  });
 
   render(listFragment);
 }
@@ -64,55 +72,68 @@ async function indexPage() {
 async function postContentPage(postId) {
   const res = await postAPI.get(`/posts/${postId}`);
   const fragment = document.importNode(templates.postContent, true);
-  fragment.querySelector('.post-content__title').textContent = res.data.title;
-  fragment.querySelector('.post-content__body').textContent = res.data.body;
-  fragment.querySelector('.post-content__back-btn').addEventListener('click', e => {
+  fragment.querySelector(".post-content__title").textContent = res.data.title;
+  fragment.querySelector(".post-content__body").textContent = res.data.body;
+  fragment.querySelector(".post-content__back-btn").addEventListener("click", e => {
     indexPage();
-  })
+    });
 
+
+    if(localStorage.getItem('token')) {
+      const commentsFargment = document.importNode(templates.comments, true);
+      const commentsRes = await postAPI.get(`/posts/${postId}/comments`);
+      commentsRes.data.forEach(comment => {
+        const ItemFragment = document.importNode(templates.commentItem, true);
+        ItemFragment.querySelector(".comment-item__body").textContent = comment.body;
+        commentsFargment.querySelector(".comments__list").appendChild(ItemFragment);
+      });
+      fragment.appendChild(commentsFargment);
+    }
   render(fragment);
 }
 
 async function loginPage() {
   const fragment = document.importNode(templates.login, true);
-  const formEl = fragment.querySelector('.login__form');
-  formEl.addEventListener('submit', async e => {
+  const formEl = fragment.querySelector(".login__form");
+  formEl.addEventListener("submit", async e => {
     // e.target.elements.username === fragment.querySelector('.login__username');
     const payload = {
       username: e.target.elements.username.value,
       password: e.target.elements.password.value
     };
     e.preventDefault();
-    const res = await postAPI.post('/users/login', payload);
+    const res = await postAPI.post("/users/login", payload);
     login(res.data.token);
     indexPage();
-  })
+  });
   render(fragment);
 }
 
 async function postFormPage() {
   const fragment = document.importNode(templates.postForm, true);
-  fragment.querySelector('.post-form__back-btn').addEventListener('click', e => {
-    e.preventDefault();
-    indexPage();
-  })
+  fragment
+    .querySelector(".post-form__back-btn")
+    .addEventListener("click", e => {
+      e.preventDefault();
+      indexPage();
+    });
 
-  fragment.querySelector('.post-form').addEventListener('submit', async e => {
+  fragment.querySelector(".post-form").addEventListener("submit", async e => {
     e.preventDefault();
     const payload = {
       title: e.target.elements.title.value,
       body: e.target.elements.body.value
     };
-    const res = await postAPI.post('/posts', payload);
+    const res = await postAPI.post("/posts", payload);
     console.log(res);
     postContentPage(res.data.id);
-  })
+  });
 
   render(fragment);
 }
 
-if (localStorage.getItem('token')) {
-  login(localStorage.getItem('token'));
+if (localStorage.getItem("token")) {
+  login(localStorage.getItem("token"));
 }
 
 indexPage();
